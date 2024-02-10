feed5862aace68ef0c5aa683b7851d5212bbef47
m0rosha <101067195+m0rosha@users.noreply.github.com>
2024-02-10 16:08:02 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='', static_folder='')
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/home/products")
+def products():
+    return render_template('./products.html')
+
+@app.route("/home/story")
+def story():
+    return render_template('./story.html')
+
+@app.route("/home/locations")
+def locations():
+    return render_template('./locations.html')
+
+@app.route("/home/careers")
+def careers():
+    return render_template('./careers.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
